73bd89a40dcd131ba632b404e5190bd49950a4f3
Verso <vuoreol@pm.me>
2024-08-16 16:55:54 +0300

    Update moderation_model.py

--- HaSpDe_SoMe/moderation_model.py
+++ HaSpDe_SoMe/moderation_model.py
@@ -1,8 +1,9 @@
+import os
 import nltk
 import requests
 import joblib
 from log import logger
-from profane_detector import ProfaneDetector
+from profane_detector import ProfaneDetector #TODO: Dont relay on this shit
 from pymongo import MongoClient
 from config import Config
 from updater import ModelUpdater
@@ -14,7 +15,7 @@ MODEL_FILE = 'moderation_model.joblib'
 MODEL_VERSION_FILE = 'model_version.txt'
 GITHUB_MODEL_URL = "https://raw.githubusercontent.com/botsarefuture/HaSpDe/main/moderation_model.joblib"
 GITHUB_VERSION_URL = "https://raw.githubusercontent.com/botsarefuture/HaSpDe/main/model_version.txt"
-LOG_SERVER_URL = "https://haspde.luova.club/log_comment"  # Replace with your server URL
+LOG_SERVER_URL = "https://S1.haspde.com/log_comment"
 
 # MongoDB setup
 client = MongoClient(config.MONGODB_URI)
@@ -36,13 +37,16 @@ def get_most_probable_class_and_percent(model, X):
     return most_probable_class_index, most_probable_percent * 100
 
 class ModerationModel:
-    def __init__(self, learns=True, human_review=False, strict_anti_profane=False, certainty_needed=80):
+    def __init__(self, learns=True, human_review=False, strict_anti_profane=False, certainty_needed=85):
         self.model = self.load_model()
         self.learns = learns
         self.human_review = human_review
-        self.profane_detector = ProfaneDetector()
-        self.strict_anti_profane = strict_anti_profane
         self.certainty_needed = certainty_needed
+        
+        if self.certainty_needed <= 79:
+            logger.error("YOU FUCKING IDIOT! IM NOW KILLING MYSELF CUZ OF YOU! PUT CERTAINTY_NEEDED TO BE OVER 80!")
+            exit()
+            #TODO: remove the whole program from user, and revoke access, this bastard is a fucking moron.
 
     def load_or_train_model(self):
         raise DeprecationWarning("This function is depraced, use load_model() instead.")
@@ -67,10 +71,6 @@ class ModerationModel:
         return model
 
     def moderate_comment(self, comment):
-        # First, check if the comment is flagged by the profane detector
-        if self.strict_anti_profane:
-            is_profane = self.profane_detector.detect_api(None, comment)
-
         # Ask for feedback before taking action
         if self.human_review:
             feedback = self.ask_for_human_review(comment)
@@ -87,32 +87,23 @@ class ModerationModel:
                 
             else:
                 logger.warning("Invalid human review input. Defaulting to model prediction.")
-                if self.strict_anti_profane:
-                    return is_profane
-
-        # If human review is disabled, proceed with automated moderation
-        if self.strict_anti_profane and is_profane:
-            self._log_comment(1, comment)
-            logger.info(f'Comment "{comment}" flagged for moderation due to profanity.')
-            return True
 
         # Use the trained model to get the most probable class and its probability
         most_probable_class, percent = get_most_probable_class_and_percent(self.model, [comment])
 
-        # Log and return based on model prediction
-        
         # We're not doing some random stuff
         if percent >= self.certainty_needed:
             if most_probable_class == 1:
                 self._log_comment(1, comment)
                 logger.info("Comment flagged for moderation.")
                 return True
+            
             elif most_probable_class == 0:
                 self._log_comment(0, comment)
                 logger.info("Comment approved.")
                 return False
         else:
-            logger.warning("Model uncertainty. Sending in for human review.")
+            logger.warning("Model uncertainty. Review required. in for human review.")
             self._human_review_db(most_probable_class, percent, comment)
             return False
 
@@ -156,12 +147,15 @@ class ModerationModel:
             'comment': comment,
             'platform': 'HaSpDe'
         }
+        
         try:
             response = requests.post(LOG_SERVER_URL, json=data)
             if response.status_code == 201:
                 logger.info("Log successfully sent to the central server.")
+                
             else:
                 logger.error(f"Failed to send log to the server. Status code: {response.status_code}")
+                
         except requests.exceptions.RequestException as e:
             logger.error(f"Error sending log to the server: {e}")
         
@@ -178,8 +172,10 @@ class ModerationModel:
                 human_input = int(input(f"Review the comment: '{comment}'\nEnter 1 to flag or 0 to approve: "))
                 if human_input in [0, 1]:
                     return human_input
+                
                 else:
                     logger.warning("Invalid input. Please enter 1 or 0.")
+                    
             except ValueError:
                 logger.warning("Invalid input. Please enter 1 or 0.")
 
@@ -187,7 +183,9 @@ if __name__ == "__main__":
     # Example usage from the command prompt
     model_instance = ModerationModel(learns=True, human_review=True)  # Set learns to True if you want to collect future training data
     while True:
+        
         comment_to_moderate = input("Enter the comment to moderate (type 'exit' to quit): ")
         if comment_to_moderate.lower() == 'exit':
             break
+        
         model_instance.moderate_comment(comment_to_moderate)
